16f448f00f65885646dc5a30adefdcc14027f1dc
Varvara <varya_lagutina@mail.ru>
2026-04-09 22:12:27 +0300

    hw les7

--- solutions/sem02/lesson07/task1.py
+++ solutions/sem02/lesson07/task1.py
@@ -1,7 +1,10 @@
+# %%
 from typing import Any
 
 import matplotlib.pyplot as plt
 import numpy as np
+
+plt.style.use("ggplot")
 
 
 class ShapeMismatchError(Exception):
@@ -13,8 +16,52 @@ def visualize_diagrams(
     ordinates: np.ndarray,
     diagram_type: Any,
 ) -> None:
-    # ваш код
-    pass
+    if abscissa.shape != ordinates.shape:
+        raise ShapeMismatchError
+
+    if diagram_type not in ["hist", "violin", "box"]:
+        raise ValueError
+
+    colors = ["#69b3a2", "#404080"]
+    labels = ["X data", "Y data"]
+
+    fig, ax = plt.subplots(figsize=(10, 6))
+
+    if diagram_type == "hist":
+        ax.hist(abscissa, bins=50, alpha=0.5, label=labels[0], color=colors[0])
+        ax.hist(ordinates, bins=50, alpha=0.5, label=labels[1], color=colors[1])
+        ax.set_title("Гистограмма распределений")
+
+    if diagram_type == "violin":
+        if diagram_type == "violin":
+            violin_parts = ax.violinplot([abscissa, ordinates], vert=False, showmedians=True)
+
+            for i, body in enumerate(violin_parts["bodies"]):
+                body.set_facecolor(colors[i])
+                body.set_edgecolor(colors[i])
+                body.set_alpha(0.6)
+
+            for part_name in violin_parts:
+                if part_name != "bodies":
+                    violin_parts[part_name].set_edgecolor(colors)
+                    violin_parts[part_name].set_linewidth(1.5)
+
+            violin_parts["cmedians"].set_linewidth(2.5)
+
+            ax.set_yticks([1, 2])
+            ax.set_yticklabels(labels)
+            ax.set_title("Плотность распределения")
+
+    if diagram_type == "box":
+        box_parts = ax.boxplot(
+            [abscissa, ordinates], labels=labels, vert=False, patch_artist=True, notch=True
+        )
+
+        for patch, color in zip(box_parts["boxes"], colors):
+            patch.set_facecolor(color)
+            patch.set_alpha(0.7)
+
+        ax.set_title("Анализ выбросов")
 
 
 if __name__ == "__main__":
@@ -25,4 +72,7 @@ if __name__ == "__main__":
     abscissa, ordinates = np.random.multivariate_normal(mean, cov, size=1000).T
 
     visualize_diagrams(abscissa, ordinates, "hist")
+    visualize_diagrams(abscissa, ordinates, "violin")
+    visualize_diagrams(abscissa, ordinates, "box")
     plt.show()
+# %%
--- solutions/sem02/lesson07/task2.py
+++ solutions/sem02/lesson07/task2.py
@@ -1,1 +1,41 @@
-# ваш код (используйте функции или классы для решения данной задачи)
+# %%
+import json
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def visualize_medic_data(f_path: str):
+    with open(f_path, "r") as f:
+        data = json.load(f)
+
+    plt.style.use("ggplot")
+    fig, ax = plt.subplots(figsize=(10, 6))
+
+    degrees_of_mitral_regurgitation = ["I", "II", "III", "IV"]
+
+    before_implant_installation = [data["before"].count(i) for i in degrees_of_mitral_regurgitation]
+    after_implant_installation = [data["after"].count(i) for i in degrees_of_mitral_regurgitation]
+
+    x = np.arange(len(degrees_of_mitral_regurgitation))
+
+    column1 = ax.bar(
+        x - 0.2, before_implant_installation, 0.4, label="До установки импланта", color="#69b3a2"
+    )
+    column2 = ax.bar(
+        x + 0.2, after_implant_installation, 0.4, label="После установки импланта", color="#404080"
+    )
+
+    ax.set_title("Эффективность импланта при митральной недостаточности")
+    ax.set_ylabel("Количество пациентов")
+    ax.set_xticks(x)
+    ax.set_xticklabels(degrees_of_mitral_regurgitation)
+    ax.legend()
+
+    ax.bar_label(column1)
+    ax.bar_label(column2)
+    plt.show()
+
+
+visualize_medic_data("medic_data.json")
+# %%
